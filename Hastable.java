3eb63fce82d145354c26fb49ce40e877de5d4aa4
Bishal Kandel <kandelbishal5610@gmail.com>
2025-01-16 19:26:41 +0545

    project add

---
+++ Hastable.java
@@ -0,0 +1,16 @@
+package Hashing;
+
+import java.util.Hashtable;
+
+public class HashingTable {
+    public static void main(String[]args){
+        Hashtable<Integer,String> hm=new Hashtable<Tnteger,String>()
+        hm.put(key:3, value:"you are visiting");
+        hm.put(key:5, value:"Hello");
+        hm.put(key:1, value:"Website");
+        hm.put(key:2, value:"java");
+
+        System.out.println(hm)
+
+    }
+}
